1a747b0cd688bb90b6882811806dd7d5c6b75fcb
Tomas Izo <tomas@izo.sk>
2016-04-20 07:17:46 +0200

    Salary calculator with overtime

---
+++ assignments/3.1/salary.py
@@ -0,0 +1,14 @@
+'''
+Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use raw_input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+'''
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+rph = raw_input("Enter Rate:")
+rph = float(rph)
+if h > 40:
+	overtime = h%40
+	h -= overtime
+	result = (h*rph)+(1.5*rph*overtime)
+else :
+	result = (h*rph)
+print result
