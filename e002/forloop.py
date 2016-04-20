04e57c571e42aae71da3a71f414fda0fefef04b3
Tomas Izo <tomas@izo.sk>
2016-04-20 06:51:22 +0200

    For Loop Example

---
+++ e002/forloop.py
@@ -0,0 +1,8 @@
+numOfCompares = 0;
+for x in range(20,0,-2) :
+	if x >= 9:
+		print x, " is greater than 9"
+	else :
+		print x, " is smaller than 9"	
+	numOfCompares += 1;
+print "Compares: ", numOfCompares		
